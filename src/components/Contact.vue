a4a898cb6cdd9e5083a4448948e2f872e4051d96
romaincapelle <romain.capelle@gmail.com>
2020-06-04 11:53:57 -0400

    not responsive

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="container max-w-2xl px-4 py-4 mx-auto">
+  <div class="container px-4 py-4 mx-auto">
     <div v-if="!orderCompleted">
       <h1 class="text-2xl ">
         Bienvenue sur le drive du magasin d’usine de La Choulette
       </h1>
-      <p class="text-sm my-2">
+      <p class="my-2">
         Ouvert du lundi au vendredi de 8h à 12h et de 14h à 18h et le samedi
         matin de 9h à 12h. Pour commander en ligne, venir retirer et payer en
         magasin, veuillez remplir le formulaire ci-dessous.
@@ -13,7 +13,7 @@
       <h2 class="text-xl mt-6">Etape 01 - Mes informations</h2>
       <form class="my-6" role="form" method="POST" @submit.prevent="sendEmail">
         <div>
-          <label class="mt-6 block text-gray-700 text-sm font-bold mb-2"
+          <label class="mt-6 block text-gray-700 font-bold mb-2"
             >Votre Nom</label
           >
           <input
@@ -25,7 +25,7 @@
           />
         </div>
         <div>
-          <label class="mt-6 block text-gray-700 text-sm font-bold mb-2"
+          <label class="mt-6 block text-gray-700 font-bold mb-2"
             >Votre Email</label
           >
           <input
@@ -37,7 +37,7 @@
           />
         </div>
         <div>
-          <label class="mt-6 block text-gray-700 text-sm font-bold mb-2"
+          <label class="mt-6 block text-gray-700 font-bold mb-2"
             >Téléphone</label
           >
           <input
@@ -50,7 +50,7 @@
         </div>
 
         <div>
-          <label class="mt-6 block text-gray-700 text-sm font-bold mb-2"
+          <label class="mt-6 block text-gray-700 font-bold mb-2"
             >Vous pouvez nous laisser un commentaire</label
           >
           <textarea
@@ -61,13 +61,13 @@
             v-model="messageMsg"
           ></textarea>
         </div>
-        <label class="my-6 block text-gray-700 text-sm font-bold mb-2"
+        <label class="my-6 block text-gray-700 font-bold mb-2"
           >Je me présente à la brasserie à la date indiqué. <br />
           {Ouvert du lundi au vendredi de 8h à 12h et de 14h à 18h et le samedi
           matin de 9h à 12h)</label
         >
         <div>
-          <label class="mt-6 block text-gray-700 text-sm font-bold mb-2"
+          <label class="mt-6 block text-gray-700 font-bold mb-2"
             >Date de passage:</label
           >
           <input
@@ -80,7 +80,7 @@
           />
         </div>
         <div>
-          <label class="mt-6 block text-gray-700 text-sm font-bold mb-2"
+          <label class="mt-6 block text-gray-700 font-bold mb-2"
             >Heure de passage:</label
           >
           <input
@@ -118,7 +118,7 @@
         </p>
         <div class="my-8">
           <button
-            class=" w-full text-sm border-2 py-1 px-2 hover:bg-white hover:text-grey bg-yellow h-12"
+            class=" w-full border-2 py-1 px-2 hover:bg-white hover:text-grey bg-yellow h-12"
             type="submit"
           >
             Finaliser la commande
