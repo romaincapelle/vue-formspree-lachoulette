56be08d0ce7b752aa7834e1049f931ec05c0615c
romaincapelle <romain.capelle@gmail.com>
2020-06-03 21:15:11 -0400

    designed with tailwind css a few more  inputs

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,101 +1,114 @@
 <template>
   <div class="container max-w-2xl px-4 py-4 mx-auto">
-    <h1 class="text-2xl ">Bienvenue sur le drive de la choulette</h1>
-    <p class="text-sm my-2">
-      Ouvert du lundi au vendredi de 8h à 12h et de 14h à 18h et le samedi matin
-      de 9h à 12h. Pour commander en ligne et venir retirer en magasin, veuillez
-      remplir le formulaire ci-dessous.
-    </p>
-    <hr class="mt-6" />
-    <h2 class="text-xl mt-6">Etape 01 - Mes informations commande</h2>
-    <form class="my-6" role="form" method="POST" @submit.prevent="sendEmail">
-      <div>
-        <label class="mt-6 block text-gray-700 text-sm font-bold mb-2"
-          >Votre Nom</label
-        >
-        <input
-          class="my-2 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          type="text"
-          name="name"
-          placeholder="Votre nom"
-          v-model="nameMsg"
-        />
-      </div>
-      <div>
-        <label class="mt-6 block text-gray-700 text-sm font-bold mb-2"
-          >Votre Email</label
-        >
-        <input
-          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          type="email"
-          name="_replyto"
-          placeholder="Votre E-mail"
-          v-model="emailMsg"
-        />
-      </div>
-      <div>
-        <label class="mt-6 block text-gray-700 text-sm font-bold mb-2"
-          >Téléphone</label
-        >
-        <input
-          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          type="email"
-          name="_replyto"
-          placeholder="Ex: 03 27 35 72 44"
-          v-model="emailMsg"
-        />
-      </div>
-      <div>
-        <label class="mt-6 block text-gray-700 text-sm font-bold mb-2"
-          >Vous pouvez nous laissez un commentaire</label
-        >
-        <textarea
-          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          name="message"
-          rows="5"
-          placeholder="Commentaire"
-          v-model="messageMsg"
-        ></textarea>
-      </div>
-
-      <BeerForm @submit="addBeer" />
-      <h3 v-if="beers.length > 0">Votre Panier</h3>
-      <ul>
-        <ListBeer
-          v-for="(beer, i) in beers"
-          :key="i"
-          :beer="beer"
-          @delete="deleteBeer(i)"
-        />
-      </ul>
-
+    <div v-if="!orderCompleted">
+      <h1 class="text-2xl ">Bienvenue sur le drive de la choulette</h1>
+      <p class="text-sm my-2">
+        Ouvert du lundi au vendredi de 8h à 12h et de 14h à 18h et le samedi
+        matin de 9h à 12h. Pour commander en ligne et venir retirer en magasin,
+        veuillez remplir le formulaire ci-dessous.
+      </p>
       <hr class="mt-6" />
+      <h2 class="text-xl mt-6">Etape 01 - Mes informations commande</h2>
+      <form class="my-6" role="form" method="POST" @submit.prevent="sendEmail">
+        <div>
+          <label class="mt-6 block text-gray-700 text-sm font-bold mb-2"
+            >Votre Nom</label
+          >
+          <input
+            class="my-2 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            type="text"
+            name="name"
+            placeholder="Votre nom"
+            v-model="nameMsg"
+          />
+        </div>
+        <div>
+          <label class="mt-6 block text-gray-700 text-sm font-bold mb-2"
+            >Votre Email</label
+          >
+          <input
+            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            type="email"
+            name="_replyto"
+            placeholder="Votre E-mail"
+            v-model="emailMsg"
+          />
+        </div>
+        <div>
+          <label class="mt-6 block text-gray-700 text-sm font-bold mb-2"
+            >Téléphone</label
+          >
+          <input
+            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            type="email"
+            name="_replyto"
+            placeholder="Ex: 03 27 35 72 44"
+            v-model="phoneMsg"
+          />
+        </div>
+        <div>
+          <label class="mt-6 block text-gray-700 text-sm font-bold mb-2"
+            >Vous pouvez nous laissez un commentaire</label
+          >
+          <textarea
+            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            name="message"
+            rows="5"
+            placeholder="Commentaire"
+            v-model="messageMsg"
+          ></textarea>
+        </div>
 
-      <!-- 18 ans > -->
-      <div class="flex mt-6">
-        <label class="flex items-center">
-          <input type="checkbox" class="form-checkbox" />
-          <span class="ml-2">Je confirme avoir plus de 18 ans.</span>
-        </label>
-      </div>
+        <BeerForm @submit="addBeer" />
+        <h3 v-if="beers.length > 0">Votre Panier</h3>
+        <ul>
+          <ListBeer
+            v-for="(beer, i) in beers"
+            :key="i"
+            :beer="beer"
+            @delete="deleteBeer(i)"
+          />
+        </ul>
 
-      <div class="my-8">
-        <button
-          class=" w-full text-sm border-2 py-1 px-2 hover:bg-white hover:text-grey bg-yellow h-12"
-          type="submit"
-        >
-          Finaliser la commande
-        </button>
+        <hr class="mt-6" />
+
+        <!-- 18 ans > -->
+        <div class="flex mt-6">
+          <label class="flex items-center">
+            <input type="checkbox" class="form-checkbox" />
+            <span class="ml-2">Je confirme avoir plus de 18 ans.</span>
+          </label>
+        </div>
+
+        <div class="my-8">
+          <button
+            class=" w-full text-sm border-2 py-1 px-2 hover:bg-white hover:text-grey bg-yellow h-12"
+            type="submit"
+          >
+            Finaliser la commande
+          </button>
+        </div>
+      </form>
+      <p class="border p-6 my-8">
+        L'ABUS D'ALCOOL EST DANGEREUX POUR LA SANTÉ, À CONSOMMER AVEC
+        MODÉRATION. BIÈRES ARTISANALES FABRIQUÉES EN FRANCE. CONTIENT DU MALT
+        D’ORGE. LA VENTE D'ALCOOL À DES MINEURS DE MOINS DE 18 ANS EST
+        INTERDITE. EN ACCÉDANT À NOS OFFRES, VOUS DÉCLAREZ AVOIR AU MOINS 18
+        ANS. LA CONSOMMATION DE BOISSONS ALCOOLISÉES PENDANT LA GROSSESSE, MÊME
+        EN FAIBLE QUANTITÉ, PEUT AVOIR DES CONSÉQUENCES GRAVES SUR LA SANTÉ DE
+        L’ENFANT.
+      </p>
+      <div>
+        <h1 class="text-2xl ">Votre Commande</h1>
+        <p>
+          Merci {{ nameMsg }}, pour votre commande. Vous avons recu un email et
+          nous vous attendons le
+        </p>
       </div>
-    </form>
-    <p class="border p-6 my-8">
-      L'ABUS D'ALCOOL EST DANGEREUX POUR LA SANTÉ, À CONSOMMER AVEC MODÉRATION.
-      BIÈRES ARTISANALES FABRIQUÉES EN FRANCE. CONTIENT DU MALT D’ORGE. LA VENTE
-      D'ALCOOL À DES MINEURS DE MOINS DE 18 ANS EST INTERDITE. EN ACCÉDANT À NOS
-      OFFRES, VOUS DÉCLAREZ AVOIR AU MOINS 18 ANS. LA CONSOMMATION DE BOISSONS
-      ALCOOLISÉES PENDANT LA GROSSESSE, MÊME EN FAIBLE QUANTITÉ, PEUT AVOIR DES
-      CONSÉQUENCES GRAVES SUR LA SANTÉ DE L’ENFANT.
-    </p>
+    </div>
+    <div v-else>
+      <p>Merci</p>
+    </div>
   </div>
 </template>
 
@@ -109,11 +122,13 @@ export default {
     return {
       nameMsg: '',
       emailMsg: '',
+      phoneMsg: '',
       messageMsg: '',
       error: false,
       beers: [],
       completed: [],
-      under18: false
+      under18: false,
+      orderCompleted: false
     }
   },
   methods: {
@@ -130,6 +145,7 @@ export default {
           message: this.messageMsg,
           beers: this.beers
         })
+        .then((this.orderCompleted = true))
         .catch(error => {
           if (error.response) {
             alert(error.response.data)
